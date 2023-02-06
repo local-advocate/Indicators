93ba4ab005beb3573787506470d63c3d4494c478
RP <rpatel18@ucsc.edu>
2023-02-06 14:17:06 -0800

    Validated arguments

--- Data Collector/data_collector.py
+++ Data Collector/data_collector.py
@@ -1,4 +1,6 @@
-""" Input Argument for Data Collector Class"""
+from datetime import datetime
+
+# Input Argument for Data Collector Class
 dataCollectorArg: dict = {
     "period": str,
     "interval": str,
@@ -7,7 +9,7 @@ dataCollectorArg: dict = {
     "end": str,
 }
 
-""" Variant values """
+''' Variant values '''
 variant: dict = {
     "V1": 1,
     "V2": 2,
@@ -16,45 +18,44 @@ variant: dict = {
     "Invalid": -1,
 }
 
-""" Default values """
+''' Default values '''
 default: dict = {"variant": variant["Invalid"], "interval": "30M"}
 
+''' Valid values '''
+valid: dict = {
+    "period": ['1d','5d','1mo','3mo','6mo','1y','2y','5y','10y','ytd','max'],
+}
 
-""" Collects data within a given period and an interval of a given ticker. """
-
-
+''' Collects data within a given period and an interval of a given ticker. '''
 class DataCollector:
 
-    """
+    '''
     Arguments:
         from     :   start date, default today - interval
         end      :   end date, default today
         period   :   period till the current date
         interval :   1M, 5M, 30M, 1H, 2H, 4H, 1D, 1W, 1M, 1Y
         ticker   :   ticker of a company
-    """
+    '''
 
     def __init__(self, args: dataCollectorArg) -> None:
-        self.variant = (
-            args["variant"] if 'variant' in args else default["variant"]
-        )  # Default variant 3
-        self.interval = (
-            args["interval"] if 'interval' in args else default["interval"]
-        )  # Default interval '30M'
+        self.variant = args["variant"] if 'variant' in args else default["variant"]        # Default variant 3
+        self.interval = args["interval"] if 'interval' in args else default["interval"]     # Default interval '30M'
         self.args = args
         # continue: validate args, py dataframes, share mem, dataclasses etc.
 
-    #
-    # Decide what parameter to collect the data
-    # Preference (highest to lowest):
-    #     1. Start, Period (Variant 1)
-    #     2. Period, End (Variant 2)
-    #     3. Period (Variant 3)
-    #     4. Start, End (Variant 4)
-    #     5. Invalid (Variant 5)
-    #
-
     def __variant(self) -> None:
+
+        '''
+        Decide what parameter to collect the data
+        Preference (highest to lowest):
+            1. Start, Period (Variant 1)
+            2. Period, End (Variant 2)
+            3. Period (Variant 3)
+            4. Start, End (Variant 4)
+            5. Invalid (Variant 5)
+        '''
+        
         if 'period' in self.args:
             if 'start' in self.args:
                 self.variant = variant["V1"]
@@ -68,11 +69,38 @@ class DataCollector:
             self.variant = variant["Invalid"]
 
 
+    def __validate(self) -> None:
+
+        ''' Validate arguments (according to variants)'''
+
+        vrt = self.variant
+        start, end = '', ''
+        if vrt == variant["Invalid"]:
+            raise IndexError('Invalid input arguments')
+        elif vrt == variant["V4"]:
+            start = datetime.strptime(self.args['start'], '%Y-%m-%d')
+            end = datetime.strptime(self.args['end'], '%Y-%m-%d')
+        if end < start:
+            raise ValueError('Start date must be before the end date')
+        else:
+            if self.args['period'] not in valid["period"]:
+                raise AssertionError('Invalid period. Valid periods include: ', valid["period"])
+            if vrt == variant["V1"]:
+                start = datetime.strptime(self.args['start'], '%Y-%m-%d')
+            elif vrt == variant["V2"]:
+                end = datetime.strptime(self.args['end'], '%Y-%m-%d')
+
+
     def run(self) -> None:
-        self.__variant()
+        ''' Main '''
+        try:
+            self.__variant()
+            self.__validate()
+        except Exception as error:
+            print('ERROR: ', error)
         
 
 
 if __name__ == "__main__":
-    d = DataCollector(args={"period": ""})
+    d = DataCollector(args={"period": "max"})
     d.run()
