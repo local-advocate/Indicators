14b4487384450e39d33294961146518f7ead7679
RP <rpatel18@ucsc.edu>
2023-02-06 15:43:07 -0800

    usage

--- Data Collector/data_collector.py
+++ Data Collector/data_collector.py
@@ -43,14 +43,15 @@ class DataCollector:
     start: str = ''
     end: str = ''
     period: str = ''
-    ticker: str = ''
+    ticker: str
     interval: str = default['interval']                                                  # Default variant 3
     _variant: str = field(init=False,repr=True,default=default['variant'])              # Default interval '30M'
 
 
     def __post_init__(self) -> None:
         object.__setattr__(self, '_variant', self.__variant())                           # Set variant
-    
+
+
     def __variant(self) -> str:
 
         '''
@@ -62,9 +63,9 @@ class DataCollector:
             4. Start, End (Variant 4)
             5. Invalid (Variant 5)
         '''
-        
+
         vrt = ''
-        
+
         if self.period:
             if self.start:
                 vrt += variant['V1']
@@ -78,6 +79,7 @@ class DataCollector:
             vrt += variant['Invalid']
 
         return vrt
+
 
     def __validate(self) -> None:
 
@@ -102,6 +104,7 @@ class DataCollector:
                 start = datetime.strptime(self.start, '%Y-%m-%d')
             elif vrt == variant['V2']:
                 end = datetime.strptime(self.end, '%Y-%m-%d')
+        return
 
 
     def run(self) -> None:
@@ -111,8 +114,35 @@ class DataCollector:
             self.__validate()
         except (AssertionError,ValueError,IndexError) as error:
             print('ERROR: ', error)
+            return
+  
+
+    def usage(self) -> None:
+        ''' Usage '''
+        info = """
+                Data Collector
+        
+        Collects the stock data of a given company (w/ a TCKR).
+        Stores it in a share memory to be used by other processes.
+        Use the run() method to run the whole program.
+        
+        Arguments:
+            1. Period       : '1d','5d','1mo','3mo','6mo','1y','2y','5y','10y','ytd','max'. 
+            2. Interval     : '1m','2m','5m','15m','30m','60m','90m','1h','1d','5d','1wk','1mo','3mo'. Default: 30m.
+            3. Start        :  yyyy-mm-dd.
+            4. End          :  yyyy-mm-dd.
+            5. Ticker*      :  TCKR of a company.
+            
+        Argument Preference/Variants (from most to least priority)
+            V1  : Start and Period provided.
+            V2  : Period and End provided.
+            V3  : Only Period provided.
+            V4  : Start and End provided.
+            V5  : All other combinations are invalid.
+        
+        """
+        print(info)
 
 if __name__ == '__main__':
-    d = DataCollector(period='1d')
-    print(d)
-    d.run()
+    d = DataCollector(period='1d', ticker='GOOGL')
+    d.usage()
