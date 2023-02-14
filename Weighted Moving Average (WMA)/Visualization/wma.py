28b96b593b943dd7ccfc9c9c0fc97ce84b075d4f
RP <rpatel18@ucsc.edu>
2023-02-13 20:53:49 -0800

    wma initialization done

--- Weighted Moving Average (WMA)/Visualization/wma.py
+++ Weighted Moving Average (WMA)/Visualization/wma.py
@@ -1,40 +1,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
-data = np.random.uniform(low=0.5, high=13.3, size=(500,))
+# data = np.random.randint(low=0, high=13, size=(10,))
 
-print(data)
+data = [100, 102, 103, 101, 104, 105, 106]
+per = 5
 
-head = 0
-len = 500
-per = 20
-sum = 0
+originalArr = np.zeros((per,))
+originalArr[0] = data[0]
+orignial_sum = originalArr[0]
+
+weightedArr = np.zeros((per,))
+weightedArr[0] = data[0]*per
+weighted_sum = weightedArr[0]
+
+weightedAvg = data[0]
+averageArr = np.zeros((len(data),))
+averageArr[0] = weightedAvg
+
 i = 1
-avg = 0
-
-wmaArray = []
-avgArray = []
-
-# Initial values
-while i <= per:
-  sum += data[i-1]
-  avg = sum / i
-  avgArray.append(avg)
-  wmaArray.append(data[i-1])
-  i += 1
-
-i -= 1
-
-while i < len:
-  sum -= wmaArray[head]
-  sum += data[i]
-  avg = sum / per
-  avgArray.append(avg)
-  wmaArray[head] = data[i]
-  head = (head+1)%per
+denom = per
+while i < per:
+  weightedArr = np.subtract(weightedArr, originalArr)
+  
+  value, weightedValue = data[i], data[i] * per
+  originalArr[i] = value
+  weightedArr[i] = weightedValue
+  
+  weighted_sum = weighted_sum - orignial_sum + weightedValue
+  orignial_sum += value
+  
+  denom += (per - i)
+  weightedAvg = weighted_sum / denom
+  averageArr[i] = weightedAvg
+  
   i += 1
   
-print(avgArray)
-
-plt.plot(data)
-plt.plot(avgArray)
-plt.show()
+print('original arr: ', originalArr)
+print('original sum: ', orignial_sum)
+print('weighted arr: ', weightedArr)
+print('weighted sum: ', weighted_sum)
+print('average arr : ', averageArr)
+print('weightedAvg : ', weightedAvg)
