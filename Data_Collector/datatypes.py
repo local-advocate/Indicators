438007382d020365a5f6d78a61dc61e7ccfeb5dc
RP <rpatel18@ucsc.edu>
2023-02-14 16:00:21 -0800

    column datatype

--- Data_Collector/datatypes.py
+++ Data_Collector/datatypes.py
@@ -12,12 +12,14 @@ default: dict = {
     'variant': variant['Invalid'],
     'interval': '30m',
     'round': 3,
-    }
+    'column': 'Open'
+}
 
 ''' Valid values '''
 valid: dict = {
     'period': ['1d','5d','1mo','3mo','6mo','1y','2y','5y','10y','ytd','max'],
     'interval': ['1m','2m','5m','15m','30m','60m','90m','1h','1d','5d','1wk','1mo','3mo'],
+    'column': ['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']
 }
 
 ''' Dataframe columns '''
